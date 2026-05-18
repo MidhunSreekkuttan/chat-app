184bed60819fc0debda9c8335d2d4d13e6da75fe
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-05-18 11:21:40 +0530

    make ready deploye functions added

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv/config'
 import dns from 'dns'
+import path from 'path'
 
 dns.setServers(["1.1.1.1", "8.8.8.8"])
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
+
+//make ready for deployment
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
+//...........................
 
 app.listen(PORT, () => {
     console.log(`server is running on port: http://localhost:${PORT}`);
