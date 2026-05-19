310b3993704c1518e78301cfea0529385aa3a920
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-05-19 22:29:27 +0530

    Craete a protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Toaster } from 'react-hot-toast'
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
+import Layout from './lib/layout'
+import LoginPage from './pages/LoginPage'
+import ChatPage from './pages/ChatPage'
+import { UserContext } from './lib/UserContext'
 
 const App = () => {
+
+  const { authState } = useContext(UserContext)
+
+  const protectedRoute = () => {
+    if (!authState) {
+      return <Navigate to="/login" />
+    }
+  }
+
   return (
     <>
 
       <Toaster />
+
+      <Routes>
+
+        <Route path='/login' element={<LoginPage />} />
+
+        <Route element={<Layout />}>
+          <Route element={protectedRoute()}>
+
+            <Route path='/' element={<ChatPage />} />
+
+          </Route>
+        </Route>
+
+      </Routes>
 
     </>
   )
