caa9bcd17e5f3d7e62b32c66c1cc1391c532925c
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-05-21 21:23:23 +0530

    SetUp tan stack query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,21 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import UserContextProvider from './lib/UserContext.jsx'
+import ChatContextProvider from './lib/ChatContext.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <UserContextProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ChatContextProvider>
+          <App />
+        </ChatContextProvider>
+      </QueryClientProvider>
     </UserContextProvider>
   </BrowserRouter>
 )
