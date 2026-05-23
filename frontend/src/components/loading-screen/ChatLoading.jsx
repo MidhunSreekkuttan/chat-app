38ef498440d0a1ed27bef6f7ea742e2785881422
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-05-23 11:00:43 +0530

    Create a chat loading screen

---
+++ frontend/src/components/loading-screen/ChatLoading.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const ChatLoading = () => {
+
+    return (
+        <div class="flex flex-col gap-4 p-5 max-w-md mx-auto animate-pulse">
+
+            <LoadingCard />
+
+            <LoadingCard />
+
+            <LoadingCard />
+
+            <LoadingCard />
+
+        </div>
+    )
+}
+
+function LoadingCard() {
+
+    return (
+        <>
+            <div className='flex flex-col gap-2 truncate'>
+                {/* <!-- Incoming message skeleton --> */}
+                <div class="flex gap-3 items-end">
+                    <div class="w-10 h-10 rounded-full bg-gray-300"></div>
+                    <div class="h-10 w-48 rounded-2xl bg-gray-300"></div>
+                </div>
+
+                {/* <!-- Incoming message skeleton (shorter) --> */}
+                <div class="flex gap-3">
+                    <div class="h-10 w-32 rounded-2xl relative left-18 bg-gray-300"></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ChatLoading
