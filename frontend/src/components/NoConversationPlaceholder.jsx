e2009d8be8ce3d5aeba2fc9dbe1e08f625a26c66
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-05-23 12:34:26 +0530

    Craete NoConversationPlaceholder component

--- frontend/src/components/NoConversationPlaceholder.jsx
+++ frontend/src/components/NoConversationPlaceholder.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { MessageCircleWarning } from 'lucide-react';
 
 const NoConversationPlaceholder = () => {
   return (
-    <div>NoConversationPlaceholder</div>
+    <div className='flex w-full h-full justify-center items-center'>
+      <div className='flex flex-col items-center gap-3'>
+
+        <div className='flex size-25 rounded-full bg-blue-600 items-center justify-center'>
+          <MessageCircleWarning size={60} className='text-slate-300' />
+        </div>
+        <h1 className='font-bold text-2xl text-slate-300'>Select a Conversation</h1>
+        <p className='text-center'>
+          Choose a Contact from the sidebar to start chatting or <br />
+          Continue a previouse conversation.
+        </p>
+      </div>
+    </div>
   )
 }
 
