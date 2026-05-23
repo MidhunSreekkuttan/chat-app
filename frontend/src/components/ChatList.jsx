e0e4713da2cd0431c43a980d92f49ba478de8bf5
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-05-23 11:01:16 +0530

    Apply chat loading screen to chatList and contactList component

--- frontend/src/components/ChatList.jsx
+++ frontend/src/components/ChatList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axiosInstance from '../lib/axiosInstance';
 import { toast } from 'react-hot-toast';
 import { useQuery } from '@tanstack/react-query';
+import ChatLoading from './loading-screen/ChatLoading';
 
 const ChatList = ({ setSeletedUser }) => {
 
@@ -28,39 +29,46 @@ const ChatList = ({ setSeletedUser }) => {
 
   return (
 
-    <div className='flex flex-col gap-2 w-full'>
+    <>
 
-      {Array.isArray(data) &&
-        data.map((item, index) => (
+      {isLoading && <ChatLoading />}
 
-          <div
-            key={item._id || index}
-            className='flex items-center gap-5 w-full p-3 hover:bg-slate-700/50
+      <div className='flex flex-col gap-2 w-full'>
+
+        {Array.isArray(data) &&
+          data.map((item, index) => (
+
+            <div
+              key={item._id || index}
+              className='flex items-center gap-5 w-full p-3 hover:bg-slate-700/50
              cursor-pointer transition-colors bg-white/10 rounded-lg'
-            onClick={() => setSeletedUser(item?._id)}
+              onClick={() => setSeletedUser(item?._id)}
 
-          >
+            >
 
-            {/* Avatar */}
-            <div className='size-14 rounded-full overflow-hidden shrink-0'>
-              <img
-                className='w-full h-full object-cover'
-                src={item?.profilePic || "/No-Pfp-Icon-Instagram.jpg"}
-                alt="User Logo"
-              />
+              {/* Avatar */}
+              <div className='size-14 rounded-full overflow-hidden shrink-0'>
+                <img
+                  className='w-full h-full object-cover'
+                  src={item?.profilePic || "/No-Pfp-Icon-Instagram.jpg"}
+                  alt="User Logo"
+                />
+              </div>
+
+              {/* Name */}
+              <div className='flex flex-col overflow-hidden'>
+                <h1 className='text-slate-300 font-semibold text-lg truncate'>
+                  {item?.name ? item?.name?.[0].toUpperCase() + item?.name?.slice(1) : ""}
+                </h1>
+              </div>
+
             </div>
+          ))}
 
-            {/* Name */}
-            <div className='flex flex-col overflow-hidden'>
-              <h1 className='text-slate-300 font-semibold text-lg truncate'>
-                {item?.name ? item?.name?.[0].toUpperCase() + item?.name?.slice(1) : ""}
-              </h1>
-            </div>
+      </div>
 
-          </div>
-        ))}
+    </>
 
-    </div>
   );
 };
 
--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react'
 import axiosInstance from '../lib/axiosInstance';
 import { toast } from 'react-hot-toast';
 import { useQuery } from '@tanstack/react-query';
+import ChatLoading from './loading-screen/ChatLoading';
 
 const ContactList = ({ setSeletedUser }) => {
 
@@ -30,39 +31,47 @@ const ContactList = ({ setSeletedUser }) => {
   })
 
   return (
-    <div className='flex flex-col gap-2 w-full'>
 
-      {Array.isArray(data) &&
-        data.map((item, index) => (
+    <>
 
-          <div
-            key={item._id || index}
-            className='flex items-center gap-5 w-full p-3 hover:bg-slate-700/50
+      {isLoading && <ChatLoading />}
+
+      <div className='flex flex-col gap-2 w-full'>
+
+        {Array.isArray(data) &&
+          data.map((item, index) => (
+
+            <div
+              key={item._id || index}
+              className='flex items-center gap-5 w-full p-3 hover:bg-slate-700/50
              cursor-pointer transition-colors bg-white/10'
-            onClick={() => setSeletedUser(item?._id)}
+              onClick={() => setSeletedUser(item?._id)}
 
-          >
+            >
 
-            {/* Avatar */}
-            <div className='size-14 rounded-full overflow-hidden shrink-0'>
-              <img
-                className='w-full h-full object-cover'
-                src={item?.profilePic || "/No-Pfp-Icon-Instagram.jpg"}
-                alt="User Logo"
-              />
+              {/* Avatar */}
+              <div className='size-14 rounded-full overflow-hidden shrink-0'>
+                <img
+                  className='w-full h-full object-cover'
+                  src={item?.profilePic || "/No-Pfp-Icon-Instagram.jpg"}
+                  alt="User Logo"
+                />
+              </div>
+
+              {/* Name */}
+              <div className='flex flex-col overflow-hidden'>
+                <h1 className='text-slate-300 font-semibold text-lg truncate'>
+                  {item?.name ? item?.name?.[0].toUpperCase() + item?.name?.slice(1) : ""}
+                </h1>
+              </div>
+
             </div>
+          ))}
 
-            {/* Name */}
-            <div className='flex flex-col overflow-hidden'>
-              <h1 className='text-slate-300 font-semibold text-lg truncate'>
-                {item?.name ? item?.name?.[0].toUpperCase() + item?.name?.slice(1) : ""}
-              </h1>
-            </div>
+      </div>
 
-          </div>
-        ))}
+    </>
 
-    </div>
   )
 }
 
