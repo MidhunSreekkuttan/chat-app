72065c1ae641bec0e7e3d46df413af9a6f1ed550
Midhun M <Midhunsreekkuttan987@gmail.com>
2026-05-24 15:53:34 +0530

    Fix axios bug while deployment

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,15 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? import.meta.env.VITE_BACKEND_URL : "",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true,
 
-    // ADD THESE HEADERS TO FIX THE VERCEL CACHE BUG
-    headers: {
-        "Cache-Control": "no-cache, no-store, must-revalidate",
-        "Pragma": "no-cache",
-        "Expires": "0"
-    }
 })
 
 export default axiosInstance;
